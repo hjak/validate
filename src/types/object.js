51f80c8c536344ccbf7e618b8f53e2d8c946cb90
evgeniy <somebody@Mac-mini-somebody.local>
2019-04-12 16:53:15 +0300

    update object

--- src/types/object.js
+++ src/types/object.js
@@ -3,8 +3,12 @@
 const { isEmpty } = require('../configuration')
 
 
-module.exports = function(schema) {
+module.exports = function(schema, { required = true }) {
   return function(data) {
+    if (!required && !data || isEmpty(data)) {
+      return null
+    }
+
     const errors = {}
     const arr = Object.keys(schema)
     for (let i = 0; i < arr.length; i++) {
